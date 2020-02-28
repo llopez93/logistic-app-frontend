051fc07e55a23756009e89faaef1c4b69653aab1
Manolo Sanhueza <manoloesanhueza@gmail.com>
2020-02-28 20:25:24 -0300

    Crea metodo findByBrand en el model.service

--- src/app/owners/services/model.service.ts
+++ src/app/owners/services/model.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
 import {GenericService} from "../../core/service/generic.service";
 import Model from '../domain/model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModelService extends GenericService<Model> {
+
+  private modelUrl = 'models';
 
   protected getResourcePath(): string {
     return "models";
@@ -13,5 +16,12 @@ export class ModelService extends GenericService<Model> {
 
   protected valueToEntity(o: any): Model {
     return new Model(o);
+  }
+
+  public findByBrand(brandId: number): Observable<Model[]> {
+    return this.doRequest({
+      url: this.baseUrl + this.modelUrl + '/by-brand/' + brandId,
+      method: 'GET',
+    });
   }
 }

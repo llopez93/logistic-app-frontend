599c7b11f445338420e0b6ef06d604c7db75a631
Manolo Sanhueza <manoloesanhueza@gmail.com>
2020-02-28 17:54:15 -0300

    Crea componente TruckForm

---
+++ src/app/owners/components/truck/truck-form/truck-form.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import {ValidationMessages} from "../../../../core/service/validation-messages";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {filter, map, switchMap} from "rxjs/operators";
+import {Observable} from "rxjs";
+
+import Truck from '../../../domain/truck';
+import {TruckService} from "../../../services/truck.service";
+import Model from '../../../domain/model';
+import {ModelService} from "../../../services/model.service";
+
+
+@Component({
+  selector: 'app-truck-form',
+  templateUrl: './truck-form.component.html',
+  styleUrls: ['./truck-form.component.scss']
+})
+export class TruckFormComponent implements OnInit {
+
+  truckForm: FormGroup;
+  modelsAsync: Observable<Model[]>;
+  validation: ValidationMessages = new ValidationMessages();
+
+  constructor(
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly fb: FormBuilder,
+    private readonly truckService: TruckService,
+    private readonly modelService: ModelService
+  ) {
+    this.truckForm = fb.group({
+      id: [null],
+      name: ["", [Validators.required, Validators.minLength(1)]],
+      domain: ["", [Validators.required, Validators.minLength(1)]],
+      year: ["", [Validators.required, Validators.minLength(1), Validators.min(1900)]],
+      model: [null, Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.modelsAsync = this.modelService.getAll();
+    const action: Observable<Params> = this.activatedRoute.params;
+    action.pipe(
+      filter(data => data.id !== "new"),
+      map(data => data.id),
+      switchMap(id => this.truckService.get(id))
+    ).subscribe(truck => {
+      this.truckForm.patchValue(truck);
+    });
+  }
+
+  saveData() {
+    let response: Observable<any>;
+    if (this.isEdition())
+      response = this.truckService.update(new Truck(this.truckForm.value));
+    else
+      response = this.truckService.create(this.truckForm.value);
+
+    response.subscribe(() =>
+      this.goBack()
+    );
+  }
+
+  compareModel(model1: Model, model2: Model): boolean {
+    return model1.compareTo(model2);
+  }
+
+  isEdition(): boolean {
+    return this.truckForm.get("id").value !== null;
+  }
+
+  goBack() {
+    this.router.navigate(["home", "owners", "trucks"]);
+  }
+
+}
